221e75453ad526a6cd3e6fedae10e9825ac56227
Christian Otu <christianotu.d@gmail.com>
2020-09-04 03:41:03 +0100

    Added App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Header} from "./components/Header";
+import "./styles/layout.css";
+import "./styles/components.css";
+
+export const App = () => {
+    return(
+        <Header/>
+    );
+};
