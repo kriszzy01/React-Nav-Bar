c17723146f076c5f02c264824de13fd7b14b5cf0
Christian Otu <christianotu.d@gmail.com>
2020-09-04 03:57:48 +0100

    Updates

---
+++ precache-manifest.8935cf8757283707ac5863d9c865020a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0b0669f128d2a7ffc1c30ee4f77ab8",
+    "url": "/React-Nav-Bar/index.html"
+  },
+  {
+    "revision": "67dd27c5f2d147e1900b",
+    "url": "/React-Nav-Bar/static/css/main.f17392f4.chunk.css"
+  },
+  {
+    "revision": "73034d9ed0fab2f451a3",
+    "url": "/React-Nav-Bar/static/js/2.8be63ede.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Nav-Bar/static/js/2.8be63ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67dd27c5f2d147e1900b",
+    "url": "/React-Nav-Bar/static/js/main.61a36f8a.chunk.js"
+  },
+  {
+    "revision": "98e4c16bac99ac1b31de",
+    "url": "/React-Nav-Bar/static/js/runtime-main.1b61095f.js"
+  }
+]);
